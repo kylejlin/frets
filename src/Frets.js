e1ffd7c01cbecd45d34decb0eb688ae3b508925c
Kyle Lin <kylejlin.us@gmail.com>
2018-04-15 23:03:33 -0700

    Refactor

--- src/Frets.js
+++ src/Frets.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Frets.css'
+import defaultSettings from './defaultSettings'
 
 const HALF_STEP = 2 ** (1 / 12)
 
@@ -8,55 +9,12 @@ class Frets extends React.Component {
     super(props)
 
     const { cellWidth, cellHeight } = this.calculateCellHeight()
+    
     this.state = {
       cellWidth,
-      cellHeight
+      cellHeight,
+      settings: defaultSettings
     }
-
-    this.stringFrequencies = [
-      82.41, // E2
-      110, // A2
-      146.8, // D3
-      196, // G3
-      246.9, // B3
-      329.6 // E4
-    ]
-    this.markings = [
-      {
-        x: 5,
-        y: 3
-      },
-      {
-        x: 7,
-        y: 1.5
-      },
-      {
-        x: 7,
-        y: 4.5
-      },
-      {
-        x: 9,
-        y: 3
-      },
-      {
-        x: 12,
-        y: 1.5
-      },
-      {
-        x: 12,
-        y: 4.5
-      },
-      {
-        x: 15,
-        y: 3
-      },
-      {
-        x: 17,
-        y: 3
-      }
-    ]
-
-    this.strings = [0, 1, 2, 3, 4, 5]
 
     const audioCtx = new AudioContext()
     const gainNode = audioCtx.createGain()
@@ -72,7 +30,9 @@ class Frets extends React.Component {
   }
 
   render() {
-    const cells = this.generateCells()
+    const cells = this.generateCells(this.state.settings)
+    const { strings, markings } = this.state.settings
+
     return (
       <div className="Frets">
         {cells.map(cell => {
@@ -82,10 +42,10 @@ class Frets extends React.Component {
               left: cell.x * this.state.cellWidth,
               top: cell.y * this.state.cellHeight
             }}
-            onClick={() => this.onCellClick(cell)}
+            onClick={() => this.onCellClick(cell, this.state.settings)}
           />
         })}
-        {this.markings.map(marking => {
+        {markings.map(marking => {
           return <div
             className="Frets-marking"
             style={{
@@ -94,11 +54,11 @@ class Frets extends React.Component {
             }}
           />
         })}
-        {this.strings.map(string => {
+        {strings.map(({}, i) => {
           return <div
             className="Frets-string"
             style={{
-              top: (string + 0.5) * this.state.cellHeight
+              top: (i + 0.5) * this.state.cellHeight
             }}
           />
         })}
@@ -124,11 +84,13 @@ class Frets extends React.Component {
     return dimensions
   }
 
-  generateCells() {
+  generateCells(settings) {
+    const { strings, frets } = settings
+    const numOfStrings = strings.length
     const cells = []
 
-    for (let i = 0; i < 6; i++) {
-      for (let j = 0; j < 21; j++) {
+    for (let i = 0; i < numOfStrings; i++) {
+      for (let j = 0; j < frets; j++) {
         cells.push({
           x: j,
           y: i
@@ -139,13 +101,13 @@ class Frets extends React.Component {
     return cells
   }
 
-  onCellClick(cell) {
+  onCellClick(cell, settings) {
     const oscillator = this.audioCtx.createOscillator()
     oscillator.connect(this.gainNode)
     oscillator.type = 'sine'
-    const stringFreq = this.stringFrequencies[cell.y]
-    const mult = HALF_STEP ** cell.x
-    oscillator.frequency.value = stringFreq * mult
+    const stringFrequency = settings.strings[cell.y]
+    const multiplier = HALF_STEP ** cell.x
+    oscillator.frequency.value = stringFrequency * multiplier
 
     oscillator.start()
     setTimeout(() => {
